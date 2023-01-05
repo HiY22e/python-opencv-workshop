347f5bc59a0c4e12d5360bad9630e2f80cdbe1d7
lgh <decadeeee>
2023-01-05 22:18:30 +0800

    GUI界面第一次编写

--- proj_01/dev.py
+++ proj_01/dev.py
@@ -5,8 +5,7 @@
 # @Software: PyCharm
 # 这是一个示例 Python 脚本。
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+
 
 
 def print_hi(name):
@@ -18,4 +17,3 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
